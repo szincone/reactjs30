ace3e3e7fcf6541e7e52e27f29e97b562c62dba4
szincone <sawyerzincone@gmail.com>
2018-10-22 14:39:50 -0600

    correctly imported the keyCodeObj from keyCodeData

--- 1-DrumKit/drum-kit/src/App.js
+++ 1-DrumKit/drum-kit/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Keys } from "./components/keys/Keys";
+import { keyCodeObj } from "./data/keyCodeData.js";
 
 class App extends Component {
+  state = {
+    keyCodeData: keyCodeObj,
+  };
   render() {
+    console.log("KEYCODE DATA", this.state.keyCodeData);
     return (
       <div className="app-wrapper">
         <Keys />
